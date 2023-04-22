3beb986f8f2b8711a24fc8923220d894e67c991c
Craig Eaton <craigea92@gmail.com>
2023-04-22 16:37:26 +0100

    Added basic styling for expense item and some information

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>28th March 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">£294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
