94cc94f9681fd0755a809dfb0cd301930c8fb7ba
Craig Eaton <craigea92@gmail.com>
2023-04-28 14:20:17 +0100

    Added a filter year function to Expenses.jsx and fixed a value in ExpensesFilter.jsx

---
+++ src/components/DummyExpenses.jsx
@@ -0,0 +1,76 @@
+const DUMMY_EXPENSES = [
+  {
+    id: "1012",
+    title: "New Television",
+    amount: 499.99,
+    date: new Date(2023, 10, 1),
+  },
+  {
+    id: "1011",
+    title: "Car Tyres",
+    amount: 144.99,
+    date: new Date(2023, 5, 18),
+  },
+  {
+    id: "1010",
+    title: "Holiday",
+    amount: 2027.99,
+    date: new Date(2023, 7, 24),
+  },
+  {
+    id: "1009",
+    title: "Computer Desk",
+    amount: 89.99,
+    date: new Date(2022, 4, 22),
+  },
+  {
+    id: "1008",
+    title: "Garden Plants",
+    amount: 56.03,
+    date: new Date(2022, 8, 14),
+  },
+  {
+    id: "1007",
+    title: "Graphics Card",
+    amount: 799.49,
+    date: new Date(2022, 3, 9),
+  },
+  {
+    id: "1006",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 11, 21),
+  },
+  {
+    id: "1005",
+    title: "Swimming Pool",
+    amount: 214.99,
+    date: new Date(2021, 9, 4),
+  },
+  {
+    id: "1004",
+    title: "Garden Shed",
+    amount: 189.99,
+    date: new Date(2021, 2, 5),
+  },
+  {
+    id: "1003",
+    title: "Sofa",
+    amount: 899.99,
+    date: new Date(2020, 11, 11),
+  },
+  {
+    id: "1002",
+    title: "Bike",
+    amount: 356.67,
+    date: new Date(2020, 1, 23),
+  },
+  {
+    id: "1001",
+    title: "Birthday Gift",
+    amount: 29.99,
+    date: new Date(2020, 6, 17),
+  },
+];
+
+export default DUMMY_EXPENSES;
